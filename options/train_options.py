ba8db101fe8841e0da04ec696f89d034b41b30ba
jakob <jens.jakob.sveding@gmail.com>
2021-05-09 15:57:31 +0200

    Money SSIM training (100)epochs

--- options/train_options.py
+++ options/train_options.py
@@ -27,8 +27,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
         # training parameters
-        parser.add_argument('--n_epochs', type=int, default=100, help='number of epochs with the initial learning rate')
-        parser.add_argument('--n_epochs_decay', type=int, default=100, help='number of epochs to linearly decay learning rate to zero')
+        parser.add_argument('--n_epochs', type=int, default=70, help='number of epochs with the initial learning rate')
+        parser.add_argument('--n_epochs_decay', type=int, default=30, help='number of epochs to linearly decay learning rate to zero')
         parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
         parser.add_argument('--gan_mode', type=str, default='lsgan', help='the type of GAN objective. [vanilla| lsgan | wgangp]. vanilla GAN loss is the cross-entropy objective used in the original GAN paper.')
@@ -36,8 +36,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
 
-        # SSIM CycleGAN
-        parser.add_argument('--cycle_loss', type=str, default='l1', help='Cycle consistency loss function [l1 | ssim]')
+        
 
         self.isTrain = True
         return parser
